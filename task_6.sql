d26e70810df4fd368653ba4b26973d298ff13ea0
Jeremy-codes <ermiasmehreteab026@gmail.com>
2025-08-29 02:15:31 +0300

    Create task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2,"Blessing Malik","bmalic@sandtech.com","125 Happines Ave."),
+VALUES (3, "Obed Ehoneah", "eobed@sandtech.com","125 Happiness  Ave."),
+VALUES (4,"Nehemial Kamolu","nkamolu@sandtech.com","126 Happiness  Ave.");
